46697a21774ee36b7aa06413b87a0817a10f6e9a
SumantChakare <sumantchakare2000@gmail.com>
2022-07-01 08:50:20 +0530

    My Travel Website

--- Traveller.js
+++ Traveller.js
@@ -39,3 +39,22 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 2500); // Change image every 2.5 seconds
 }
+
+var mybutton = document.getElementById("srl_up");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
